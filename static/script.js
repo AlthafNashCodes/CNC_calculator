caf6c33c6db672a8bb4ac8e98ae4fb1976973f75
Mohammed-Althaf <mohamedalthaf694@gmail.com>
2024-08-18 13:00:59 +0400

    Added total section

--- static/script.js
+++ static/script.js
@@ -118,10 +118,10 @@ document.getElementById("Thick-dr-c1").addEventListener("click", function() {
     }
 });
 
-document.getElementById("weight-btn").addEventListener("click", function() {
-    let weight = parseFloat(document.getElementById("weight-input").value);
+document.getElementById("weight-btn2").addEventListener("click", function() {
+    let weight = parseFloat(document.getElementById("weight-input2").value);
     let cost_per_pounds = weight * 0.59;
-    document.getElementById("Weight").innerHTML = cost_per_pounds.toFixed(2);
+    document.getElementById("Weight-label2").innerHTML = cost_per_pounds.toFixed(2);
 });
 
 document.getElementById("materials-btn").addEventListener("click", function () {
@@ -150,9 +150,43 @@ document.getElementById("Add-labour-btn").addEventListener("click", function () 
     document.getElementById("lab-metrial_label").innerHTML = Labourin;
 });
 
+document.getElementById("month-btn").addEventListener("click", function () {
+    const Labourin2 = parseFloat(document.getElementById("month-input4").value) || 0;
+    document.getElementById("month_label").innerHTML = Labourin2;
+});
+
 document.getElementById("Reduce-labour-btn").addEventListener("click", function () {
     const Labourin2 = parseFloat(document.getElementById("Labour-input4").value) || 0;
     document.getElementById("reduce-lab-metrial_label").innerHTML = Labourin2;
 });
+
+document.getElementById("total-btn").addEventListener("click", function () {
+
+    let weight = document.getElementById("Weight-label2")
+
+    let Added_cost = parseFloat(document.getElementById("metrial_label").innerHTML) || 0;
+    let Reduced_cost = parseFloat(document.getElementById("r-metrial_label").innerHTML) || 0;
+
+    let Added_labour = parseFloat(document.getElementById("lab-metrial_label").innerHTML) || 0 + weight;
+    let Reduced_Labour = parseFloat(document.getElementById("reduce-lab-metrial_label").innerHTML) || 0 + weight;
+
+    let No_of_months = parseFloat(document.getElementById("month_label").innerHTML) || 0;
+
+    let Labour_saving = Added_labour - Reduced_Labour;
+    let material_saving = Reduced_cost - Added_cost ;
+
+    let saving_per_year = material_saving + Labour_saving;
+
+    let monthly_saving = saving_per_year / 12;
+    let FYE = No_of_months * monthly_saving
+    
+    document.getElementById("total-lab-metrial_label1").innerHTML = saving_per_year.toFixed(2);
+    document.getElementById("total-lab-metrial_label2").innerHTML = (saving_per_year * 12).toFixed(2); // Assuming yearly saving
+    document.getElementById("total-lab-metrial_label3").innerHTML = monthly_saving.toFixed(2);
+    document.getElementById("total-lab-metrial_label4").innerHTML = FYE.toFixed(2);
+});
+
+
+
 
 
